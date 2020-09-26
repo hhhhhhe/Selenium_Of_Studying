e570aeb4d5b64e5fc331a9d74474b7c0e79ce1b0
hhhhhhe <1668051992@qq.com>
2020-09-26 19:16:58 +0800

    打开浏览器的方法

--- start_browser.py
+++ start_browser.py
@@ -1,4 +1,8 @@
-#coding=utf-8
+# coding=utf-8
 from selenium import webdriver
-driver = webdriver.Chrome()
+# 启动chrome游览器
+# driver = webdriver.Chrome()
+# driver.get("http://www.baidu.com")
+# 启动edge游览器
+driver = webdriver.Firefox()
 driver.get("http://www.baidu.com")
