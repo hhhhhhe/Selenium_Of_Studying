76cb39df3581f1ad6ecc891b1d4d0c63b3bbeeb7
hhhhhhe <1668051992@qq.com>
2020-09-28 14:26:44 +0800

    定位裁剪图片

--- start_browser.py
+++ start_browser.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 import time
 import random
+from PIL import Image
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -11,6 +12,7 @@ driver = webdriver.Chrome()
 # 启动edge游览器
 # driver = webdriver.Firefox()
 driver.get("http://www.5itest.cn/register")
+driver.maximize_window()
 # 延迟五秒等这个页面加载完
 time.sleep(5)
 # 页面加载完后检测页面有没有“包含注册字样”，来判断界面是否加载成功
@@ -23,17 +25,34 @@ locator = (By.CLASS_NAME, "controls")
 # 如果找到元素则往下运行，没有则返回false
 WebDriverWait(driver, 1).until(EC.visibility_of_element_located(locator))
 email_element = driver.find_element_by_id("register_email")
+# 保存界面上的图片
+driver.save_screenshot("E:/imooc.png")
+code_element = driver.find_element_by_id("getcode_num")
+# 打印出图片在界面上的坐标位置
+print(code_element.location)  # {"x":123,"y":345}
+# 图片左上角坐标
+left = code_element.location['x']
+top = code_element.location['y']
+
+# 图片右下角坐标
+right = code_element.size['width']+left
+height = code_element.size['height']+top
+print(left, top, right, height)
 # 获取元素的值
-print(email_element.get_attribute("placeholder"))
-email_element.send_keys("test@163.com")
+# print(email_element.get_attribute("placeholder"))
+# email_element.send_keys("test@163.com")
 # 获取用户填入的值
-print(email_element.get_attribute("value"))
-
-
+# print(email_element.get_attribute("value"))
+# 打开图片
+im = Image.open("E:/imooc.png")
+# 裁剪图片
+img = im.crop([1001, 650, 1157, 720])
+# 将裁剪获得的图片保存为imooc1.png
+img.save("E:/imooc1.png")
 # 自动生成邮箱账户
-for i in range(5):
-    user_email = ''.join(random.sample('1234567890avcah', 5))+"@163.com"
-    print(user_email)
+# for i in range(5):
+#     user_email = ''.join(random.sample('1234567890avcah', 5))+"@163.com"
+#     print(user_email)
 
 
 # 每次实例化一个driver但没有关闭的话会导致电脑越来越卡顿，所以应该关闭掉driver
